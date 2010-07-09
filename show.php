a726decba98f431f19bdd20d69431f4b498599e7
SosoBz <SosoBz@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-07-09 13:19:02 +0000

    upload
    git-svn-id: https://plugins.svn.wordpress.org/sosobz-short-url/trunk@261954 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- show.php
+++ show.php
@@ -1,5 +1,5 @@
 <div style="margin-top:2em;">
 	<?php echo __("<font size='4'>* A <a href='http://soso.bz' style='text-decoration:none;' target='_blank'> shorter link</a> for this article:&nbsp;&nbsp;</font>") ?>
 	<input type="text" size="25" id="SosoBz" value="<?php echo get_post_meta($post->ID, 'SosoBzShortURL', true); ?>">&nbsp;&nbsp;
-	<p><a target="_blank" href="http://www.addthis.com/bookmark.php?url=<?php echo get_post_meta($post->ID, 'SosoBzShortURL', true); ?>"><img src="http://s7.addthis.com/static/btn/v2/lg-share-en.gif"></a></p>
+	<p><a target="_blank" href="http://www.addthis.com/bookmark.php?url=<?php echo get_post_meta($post->ID, 'SosoBzShortURL', true); ?>"><img src="http://soso.bz/jscss/bookmark.gif"></a></p>
 </div>
